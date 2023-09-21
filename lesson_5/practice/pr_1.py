85a8ded6ef3a395360c8c178b483bb38a81ebc40
Aliaksandr Malakhau <altius200@gmail.com>
2023-09-21 22:24:51 +0300

    lesson 4

---
+++ lesson_5/practice/pr_1.py
@@ -0,0 +1,68 @@
+# task 1
+
+def hello():
+    print('Hello, world!')
+
+
+hello()
+
+
+# task 2
+
+def return_hello():
+    return 'Hello, world! task 2'
+
+
+print(return_hello())
+
+
+# task 3
+
+def hello_name(name):
+    print(f'Hello {name}')
+
+
+hello_name('Alexander')
+
+
+# task 4
+def ret_hello_name(name):
+    return f'Hello {name} from task 4'
+
+
+# task 5
+def is_polindrome(text):
+    return text == text[::-1]
+
+
+print(is_polindrome('lol'))
+
+
+# task 6
+
+def sum_list(numbers: list):
+    result = 0
+    for i in numbers:
+        result += i
+    return result
+
+
+print(sum_list([1, 2, 3, 4, 3, 2, 2]))
+
+
+# 7
+def sum_and_max(*numbers):
+    return (f'sum {sum([i for i in numbers])}, max {max(numbers)}')
+
+
+print(sum_and_max(1, 2, 3, 4, 5, 6, 4, 3, 8, 11))
+
+
+# 8
+def my_round(number: float, round_number):
+    number = str(number)
+    ind = number.index('.')
+    new_number = number[:ind + round_number]
+
+
+print(my_round(5.432,2))

74ffd3e7f67aad099e3dd833fbaf59400bc6aa7b
Aliaksandr Malakhau <altius200@gmail.com>
2023-10-01 23:51:15 +0300

    checking of symbols was added to 1.4

--- lesson_5/home_tasks/home_task_1.4.py
+++ lesson_5/home_tasks/home_task_1.4.py
@@ -1,13 +1,16 @@
 # the function is accepting a text and returns the longest word
-
+alphabet = 'abcdefghijklmnopqrstuvwxyz '
 
 def get_longest_word(text: str) -> str:
     text = text.lower().split(' ')
     biggest_word = ''
     for word in text:
-        if len(word) > len(biggest_word):
-            biggest_word = word
+        for letter in word:
+            if letter not in alphabet:
+                return 'Invalid input, there are non-alphabetical characters in the text'
+            elif len(word) > len(biggest_word):
+                biggest_word = word
     return biggest_word
 
 
-print(get_longest_word('hello this is a string with words and spaces and big big woooooooooord'))
+print(get_longest_word('hello0 this is a string with words and spaces and big big woooooooooord'))

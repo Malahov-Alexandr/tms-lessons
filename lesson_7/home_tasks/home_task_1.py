bcfc5b6c989142d1ef919b9934e37c66ab0968d4
Aliaksandr Malakhau <altius200@gmail.com>
2023-10-05 18:35:40 +0300

    edited home tasks

--- lesson_7/home_tasks/home_task_1.py
+++ lesson_7/home_tasks/home_task_1.py
@@ -1,6 +1,7 @@
 def map_to_tuples(text):
     return list(map(lambda x: (x.upper(), x.lower()), text))
 
+
 user_input = input('Enter your text: ').lower().split()
 
 print(map_to_tuples(user_input))

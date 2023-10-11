119052587c94456b9cafb3d5252433a814fe2485
Aliaksandr Malakhau <altius200@gmail.com>
2023-10-11 23:41:16 +0300

    big home_work editet version

--- big_homework/bank.py
+++ big_homework/bank.py
@@ -1,5 +1,6 @@
-wimport json
+import json
 import random
+import os
 
 
 def get_random_digits(count: int) -> str:
@@ -8,92 +9,107 @@ def get_random_digits(count: int) -> str:
 
 class BankAccount:
 
-    def __init__(self, card_holder: str):
+    def __init__(self, card_holder, money=0.0, card_number=None, account_number=None):
         self.card_holder = card_holder.upper()
-        self.money = 0
-        self.account_number = get_random_digits(20)
-        self.card_number = get_random_digits(16)
+        self.money = money
+        self.account_number = get_random_digits(20) if account_number is None else account_number
+        self.card_number = get_random_digits(16) if card_number is None else card_number
+
+
+def convert_bank_account_to_dict(bank_account: BankAccount):
+    return {'card_holder': bank_account.card_holder,
+            'money': bank_account.money,
+            'account_number': bank_account.account_number,
+            'card_number': bank_account.card_number}
+
+
+def load_accounts(file_name) -> dict[str, BankAccount]:
+    if not os.path.exists(file_name):
+        return {}
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+        return {account_number: BankAccount(**data) for account_number, data in data.items()}
+
+
+def save_accounts(file_name, bank_accounts):
+    with open(file_name, 'w') as file:
+        json.dump({account_number: convert_bank_account_to_dict(account) for account_number, account in bank_accounts.items()}, file, indent=4)
 
 
 class Bank:
 
-    def __init__(self):
-        self.bank_accounts = {}
+    def __init__(self, bank_accounts=None):
+        self.bank_accounts = {} if bank_accounts is None else bank_accounts
 
-    def open_account(self, card_holder: str):
-        person = BankAccount(card_holder)
-        #TODO add .__dict__ after person
-        self.bank_accounts[person.account_number] = person
-        print(self.bank_accounts)
-        #json was created for debugging
-        # self.writer(self.bank_accounts)
-        return person
+    def open_account(self, card_holder):
+        account = BankAccount(card_holder)
+        self.bank_accounts[account.account_number] = account
+        return account
 
-    def add_money(self, account_number: str, money: int):
+    def add_money(self, account_number, money):
         self.bank_accounts[account_number].money += money
 
-    def transfer_money(self, account_number_from: str, account_number_to: str, money: int):
-        self.bank_accounts[account_number_from].money -= money
-        self.bank_accounts[account_number_to].money += money
+    def transfer_money(self, from_account_number, to_account_number, money):
+        self.bank_accounts[from_account_number].money -= money
+        self.bank_accounts[to_account_number].money += money
 
-    def external_transfer(self, account_number, to_external_account, money):
-        self.bank_accounts[account_number].money -= money
-        print(f'Банк перевёл {money}$ с вашего счёта {account_number} на внешний счёт {to_external_account}')
+    def external_transfer(self, from_account_number, to_external_number, money):
+        self.bank_accounts[from_account_number].money -= money
+        print(f'Банк перевёл {money} $ с вашего счёта {from_account_number} на внешний счёт {to_external_number}')
 
-    def show_accounts(self):
+    def all_accounts(self):
         print('Ваши открытые счета:')
         for account_number, account in self.bank_accounts.items():
             print(f'Cчёт: {account_number}')
-            print(f'   Остаток на счету: {account.money}$')
-            print(f'   Номер карты: {account.card_number}')
-            print(f'   Держатель карты: {account.card_holder}')
+            print(f' Остаток на счету: {account.money} $')
+            print(f' Номер карты: {account.card_number}')
+            print(f' Держатель карты: {account.card_holder}')
 
-    def writer(self,data):
-        with open('test.json','w') as f:
-            json.dump(data, f, indent=4)
+
 class Controller:
 
-    def __init__(self):
+    def __init__(self, data_file_name=None):
         self.bank = Bank()
+        self.data_file_name = 'data.json' if data_file_name is None else data_file_name
+        self.bank.bank_accounts = load_accounts(self.data_file_name)
 
     def run(self):
-        print("Здравствуйте, наш банк открылся!")
+        print('Здравствуйте, наш банк открылся!')
         while True:
-            user_input = int(input("""
-            Выберите действие:
-            0. Завершить программу
-            1. Открыть новый счёт
-            2. Просмотреть открытые счета
-            3. Положить деньги на счёт
-            4. Перевести деньги между счетами
-            5. Совершить платёж
-            """))
+            print("""Выберите действие:
+           0. Завершить программу
+           1. Открыть новый счёт
+           2. Просмотреть открытые счета
+           3. Положить деньги на счёт
+           4. Перевести деньги между счетами
+           5. Совершить платёж""")
+            user_input = int(input())
             if user_input == 0:
-                print("До свидания!")
+                print('До свидания!')
+                save_accounts(self.data_file_name, self.bank.bank_accounts)
                 break
             elif user_input == 1:
-                user_name = input('Введите имя и фамилию держателя карты (на английском): ')
-                account = self.bank.open_account(user_name)
-                print(f'Счёт {account.account_number} создан.')
+                user_name = input('Enter your name: ')
+                new_account = self.bank.open_account(user_name)
+                print(f'"Счёт {new_account.account_number} создан"')
             elif user_input == 2:
-                self.bank.show_accounts()
+                self.bank.all_accounts()
             elif user_input == 3:
-                account_number = (input('Введите номер cчёта: '))
+                account_number = input('Введите номер cчёта:  ')
                 money = int(input('Количество денег: '))
                 self.bank.add_money(account_number, money)
             elif user_input == 4:
-                pass
-            # TODO send money from one account ot another
+                from_account = input('Введите номер cчёта-отправителя: ')
+                recipient_account = input('Введите номер cчёта-получателя: ')
+                money = int(input('Количество денег: '))
+                self.bank.transfer_money(from_account, recipient_account, money)
             elif user_input == 5:
-                pass
-            # TODO do a payment from an account
-            else:
-                pass
-            # TODO add descripiption that the user entered wrong number
-
-    # logger for logging
+                from_account = input('Введите номер cчёта-отправителя: ')
+                external_user = input('Введите номер счёта внешнего получателя: ')
+                money = int(input('Количество денег: '))
+                self.bank.external_transfer(from_account, external_user, money)
 
 
 if __name__ == '__main__':
-    controller = Controller()
+    controller = Controller(data_file_name='data.json')
     controller.run()

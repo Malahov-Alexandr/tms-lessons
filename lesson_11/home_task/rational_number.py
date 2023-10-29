41775b9fe85278f58daed8e330c247febd6842fb
Aliaksandr Malakhau <altius200@gmail.com>
2023-10-29 22:45:57 +0200

    practice generator

--- lesson_11/home_task/rational_number.py
+++ lesson_11/home_task/rational_number.py
@@ -13,11 +13,11 @@ class Rational:
 
 
     # TODO
-    def __add__(self, otherfraction):
-        newnum = self.num * otherfraction.den + self.den * otherfraction.num
-        newden = self.den * otherfraction.den
-
-        return Fraction(newnum, newden)
+    # def __add__(self, otherfraction):
+    #     newnum = self.num * otherfraction.den + self.den * otherfraction.num
+    #     newden = self.den * otherfraction.den
+    #
+    #     return Fraction(newnum, newden)
 
 r = Rational(1,2)
 print(r)

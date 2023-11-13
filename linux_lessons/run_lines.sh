be9ae8d085ae311a8603bc7ef72c72ee14c9ea48
Aliaksandr Malakhau <altius200@gmail.com>
2023-11-13 01:34:17 -0500

    auto result

--- linux_lessons/run_lines.sh
+++ linux_lessons/run_lines.sh
@@ -1,6 +1,6 @@
 input_file="code_lines.txt"
 
-
+python3 lazy_commands.py
 # Loop through each line in the file and execute the command
 while IFS= read -r command; do
     # Execute the command and append the output to the output file

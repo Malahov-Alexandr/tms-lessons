100c0e541b6915493af3babffc21fdbbec352aea
Aliaksandr Malakhau <altius200@gmail.com>
2023-11-26 15:18:53 +0200

    linux filder was deleted

--- linux_lessons/run_lines.sh
+++
@@ -1,8 +0,0 @@
-input_file="code_lines.txt"
-
-python3 lazy_commands.py
-# Loop through each line in the file and execute the command
-while IFS= read -r command; do
-    # Execute the command and append the output to the output file
-    eval "$command"
-done < "$input_file"
